547b2574b3510657ffb19b0b4dd4340ef188b992
Sarah Zaki <sarahzaki03@gmail.com>
2019-04-09 13:55:51 +0300

    Front end Project

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<!-- Index File -->
+<!DOCTYPE html>
+<html>
+
+<head> 
+
+	<title> Donors Site </title>
+
+	<!-- Style Links -->
+	<link  rel="stylesheet" href="css/main.css"/> 
+	
+	<!-- JQuery Part -->
+	<script src="js/jquery.min.js"></script>
+	
+	
+</head>
+<body>
+
+</body>
+</html>
